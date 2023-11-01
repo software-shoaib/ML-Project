113d73fa9b30008d0e506ed97396f91394f3e867
Software-shoaib <software.shoaib@gmail.com>
2023-11-01 17:31:09 +0500

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
@@ -17,6 +17,12 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
+if __name__== "__main__":
+    
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Devide by zero Error!")
+        raise CustomException(e,sys)
 
         
